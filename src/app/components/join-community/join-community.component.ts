9e5e2eae1fd13b3cb8e89f51d8d6b0c293097036
Ahmed Elbassal <ahmed.bassal.italy@gmail.com>
2021-07-02 07:06:08 +0200

    ddd

---
+++ src/app/components/join-community/join-community.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-join-community',
+  templateUrl: './join-community.component.html',
+  styleUrls: ['./join-community.component.css']
+})
+export class JoinCommunityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
