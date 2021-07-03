228ac5af65c3f48f9d79b770d759edfcea7edd2b
Ahmed Elbassal <ahmed.bassal.italy@gmail.com>
2021-07-03 05:01:50 +0200

    routes for user edit details and password

--- src/app/components/Navbar/navbar/navbar.component.ts
+++ src/app/components/Navbar/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonsAmongAllUsersService } from 'src/app/services/commons-among-all-users.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:CommonsAmongAllUsersService,private router:Router) { }
 
   ngOnInit(): void {
+
+    let userType=localStorage.getItem("devCommunityUserType");
+
+    this.subscriber=this.userService.getMyDetailsByToken(userType).subscribe(
+      (data)=>{
+        console.log(data)
+
+        this.userDetials=data;
+
+        this.loggedIn=true;
+
+      },
+      (err)=>{
+        console.log(err)
+     
+      },
+      ()=>{
+        this.subscriber.unsubscribe();
+    
+       
+
+
+      }
+    )
+
+
+
+  }
+
+  subscriber:any;
+  userDetials:any;
+  loggedIn:boolean=false;
+
+
+  navigateUserEditDetails(){
+    this.router.navigateByUrl("user/edit")
+  }
+
+  navigateUserEditPassword(){
+    this.router.navigateByUrl("user/password")
+  }
+
+  navigateUserDeactivate(){
+    this.router.navigateByUrl("user/deactivate")
   }
 
 }
